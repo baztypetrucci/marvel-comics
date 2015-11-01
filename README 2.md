0e4f5c8f95835464b98fa7cb2996e4afd91e5b85
Bastián Hidalgo <Bastian@Air-de-Bastian.iptv.microsoft.com>
2015-11-01 17:12:52 -0300

    commit nuevo

---
+++ README 2.md
@@ -0,0 +1,2 @@
+# Marvel-Comics
+App desarrollada para demostrar habilidades en Front End utilizando GitHub, Ionic, Gulp y Heroku
--- www/js/app.js
+++ www/js/app.js
@@ -3,82 +3,77 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-// 'starter.services' is found in services.js
-// 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+var app = angular.module('marvelComics', ['ionic']);
+app.config(function($stateProvider, $urlRouterProvider) {
+	$urlRouterProvider.otherwise('/');
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})
-
-.config(function($stateProvider, $urlRouterProvider) {
-
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
-
-    // setup an abstract state for the tabs directive
-    .state('tab', {
-      url: "/tab",
-      abstract: true,
-      templateUrl: "templates/tabs.html"
-    })
-
-    // Each tab has its own nav history stack:
-
-    .state('tab.dash', {
-      url: '/dash',
-      views: {
-        'tab-dash': {
-          templateUrl: 'templates/tab-dash.html',
-          controller: 'DashCtrl'
+	$stateProvider.state('home', {
+		url: '/',
+		templateUrl: 'home.html',
+		controller: 'HomeCtrl'
+	})
+    .state('search', {
+		url: '/search',
+        templateUrl: 'search.html',
+        controller: 'SearchCtrl'
+	})
+    .state('search.personaje', {
+		url: '/personaje',
+        views:{
+            'personaje-tab':{
+                templateUrl: 'personaje.html',
+                controller: 'SearchCtrl'
+            }
         }
-      }
-    })
-
-    .state('tab.friends', {
-      url: '/friends',
-      views: {
-        'tab-friends': {
-          templateUrl: 'templates/tab-friends.html',
-          controller: 'FriendsCtrl'
+	})
+    .state('search.comics', {
+		url: '/comics',
+        views:{
+            'comics-tab':{
+                templateUrl: 'comics.html',
+                controller: 'SearchCtrl'
+            }
         }
-      }
-    })
-    .state('tab.friend-detail', {
-      url: '/friend/:friendId',
-      views: {
-        'tab-friends': {
-          templateUrl: 'templates/friend-detail.html',
-          controller: 'FriendDetailCtrl'
+	});
+});
+app.factory('Busqueda', function() {
+	busqueda = [];
+	busqueda.nombre = '';
+	return busqueda;
+});
+app.controller('HomeCtrl', function($scope, Busqueda) {
+	$scope.input = Busqueda;
+});
+app.controller('SearchCtrl', function($scope, Busqueda, $http) {
+    $scope.input = Busqueda;
+    //console.log(Busqueda.nombre);
+    var idPersonaje;
+    $http.get('http://gateway.marvel.com/v1/public/characters?apikey=fd5c2ffa2af583c51659be24b41e1203&ts=9&hash=74f732251e16455f8c07e261d31eaa7f&name='+Busqueda.nombre).success(function(data) {
+    //$http.get('../spider-man.json').success(function(data) {
+        $scope.Personaje = data.data.results;
+        for (var i = 0; i < data.data.results.length; i++) {
+            idPersonaje = data.data.results[i].id;
+            $http.get('http://gateway.marvel.com/v1/public/characters/'+idPersonaje+'/comics?apikey=fd5c2ffa2af583c51659be24b41e1203&ts=9&hash=74f732251e16455f8c07e261d31eaa7f').success(function(data) {
+                $scope.Comics = data.data.results;
+            });
         }
-      }
-    })
-
-    .state('tab.account', {
-      url: '/account',
-      views: {
-        'tab-account': {
-          templateUrl: 'templates/tab-account.html',
-          controller: 'AccountCtrl'
-        }
-      }
     });
 
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
-
 });
-
+app.controller('BackCtrl', function($scope, $ionicHistory){
+    $scope.myGoBack = function() {
+        $ionicHistory.goBack();
+    };
+});
+app.run(function($ionicPlatform) {
+	$ionicPlatform.ready(function() {
+		// Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+		// for form inputs)
+		if (window.cordova && window.cordova.plugins.Keyboard) {
+			cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+		}
+		if (window.StatusBar) {
+			StatusBar.styleDefault();
+		}
+	});
+});
